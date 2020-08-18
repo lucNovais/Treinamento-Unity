b31e9002511452aeed66a6755f72ef7bda9278df
jjader <jjader03@gmail.com>
2020-08-18 04:53:26 -0300

    adicionando script

---
+++ TutorialLocobots/Assets/Scripts/Cubo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubo : MonoBehaviour
+{
+    public Vector3 aux;
+    Rigidbody cuboRigibody;
+
+    void Start()
+    {
+        cuboRigibody = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        Movimenta();
+    }
+
+    void Movimenta()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            aux = cuboRigibody.velocity;
+            aux.x = -10;
+            cuboRigibody.velocity = aux;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            aux = cuboRigibody.velocity;
+            aux.x = 10;
+            cuboRigibody.velocity = aux;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            aux = cuboRigibody.velocity;
+            aux.z = 10;
+            cuboRigibody.velocity = aux;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            aux = cuboRigibody.velocity;
+            aux.z = -10;
+            cuboRigibody.velocity = aux;
+        }
+    }
+}
---
+++ TutorialLocobots/Assets/Scripts/Sensor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{
+    public bool sensor = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.CompareTag("line"))
+        {
+            sensor = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.CompareTag("line"))
+        {
+            sensor = false;
+        }
+    }
+
+}
